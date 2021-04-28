89e7aef83df1feeef25f77b0fb27e3a6a0cb9c33
PureObstacles <82127699+PureObstacles@users.noreply.github.com>
2021-04-28 16:30:11 -0700

    Add files via upload
    For the single_pure.py that one is used to send induvial claims to a Discord via webhook. For the multi_pure.py takes a usernames.txt which will send all those claims in that txt file to a Discord via webhook. 
    
    Their is 2 webhook links, used for if you like to post your claims in 2 servers. (Added on both single_pure.py & multi_pure.py)

---
+++ single_pure.py
@@ -0,0 +1,53 @@
+import json
+import requests
+import colorama
+import discord
+from discord import Webhook, AsyncWebhookAdapter
+import aiohttp
+import asyncio
+import time
+import os
+from colorama import Fore, init, Style
+init(convert=True)
+
+ERROR = "[\x1b[31m-\x1b[39m]"
+SUCCESS = "[\x1b[32m+\x1b[39m]"
+INPUT = "[\x1b[33m?\x1b[39m]"
+INFO = "[\x1b[35m>\x1b[39m]"
+RED = "\033[1;31;40m"
+GREEN = "\033[1;32;40m"
+BLUE = "\033[1;36;40m"
+WHITE = "\033[1;37;40m"
+LIGHTMAGENTA = "\033[1;35;40m"
+YELLOW = Fore.LIGHTYELLOW_EX
+
+webhook_url_1 = 'WEBHOOK LINK FOR 1ST'
+webhook_url_2 = 'WEBHOOK LINK FOR 2ND'
+
+
+async def send_webhook():
+
+    global webhook_url_1
+    global webhook_url_2
+    
+    async with aiohttp.ClientSession() as session:
+        t = time.localtime()
+        current_time = time.strftime("%H:%M:%S", t)
+        os.system('cls')
+        
+        x = input("Username: ")
+        webhook = Webhook.from_url(webhook_url_1, adapter=AsyncWebhookAdapter(session))  
+        webhook1 = Webhook.from_url(webhook_url_2, adapter=AsyncWebhookAdapter(session)) 
+        embed=discord.Embed(url="https://twitter.com/{}".format(x), title="@{}".format(x.strip("\r\n")), description="𝙇𝙤𝙤𝙠𝙞𝙣𝙜 𝙛𝙤𝙧 𝙤𝙛𝙛𝙚𝙧𝙨!", color=1127128)
+        embed.set_author(name="Pure Twitter Claims")
+        embed.set_footer(text="Obstacles#1555 - too#1001 * {}".format(current_time))
+        embed.set_thumbnail(url='https://media.giphy.com/media/MNa0HKdhc3SGQ/giphy.gif')
+        await webhook.send(embed=embed, username='Pure')
+        await webhook1.send(embed=embed, username='Pure')
+        time.sleep(2)
+        print("@{} has been claimed!".format(x))
+        time.sleep(5)
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(send_webhook())
